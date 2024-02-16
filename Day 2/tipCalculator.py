4c0d1132a79a5f67a8dd037cc52410710e0aac1c
aseifi880 <aseifi880@gmail.com>
2024-02-16 18:52:25 +0330

    day2 project finished

--- Day 2/tipCalculator.py
+++ Day 2/tipCalculator.py
@@ -1,1 +1,6 @@
-print("Hello world")
+print("Welcome to the tip calculator.")
+billAmount = float(input("what was the total bill? "))
+tipPercent = float(input("what percent tip would you like to give? "))
+numPeople = float(input("how many people to split the bill with? "))
+sharePerPerson = ( billAmount * ( 1 + tipPercent / 100 ) ) / numPeople
+print(f"Each person should pay : {round(sharePerPerson, 2)}")
